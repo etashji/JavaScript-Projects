8adbfa9851ddd3720d01010d88ddd30dedd8360c
etashji <99293073+etashji@users.noreply.github.com>
2022-02-17 12:26:44 -0500

    added fontcolor

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,6 +3,7 @@ document.write("JK I don't know how to do that. ") //This is a basic script
 
 var hacked = "You are not hacked." //This defines the variable "hacke"
 var safe = "You are safe. " //This defines the variable "safe"
+hacked.fontcolor("green")//This turns the hacked variable green.
 document.write(hacked + safe); //This is meant to concatenate the "hacked" and "safe" variables.
 
 document.write(1+2) // This is an expression.

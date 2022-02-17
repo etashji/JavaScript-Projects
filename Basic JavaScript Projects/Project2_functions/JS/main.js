0a074f1a3628a43748959cd67d08f101fc83f81e
etashji <99293073+etashji@users.noreply.github.com>
2022-02-17 16:05:09 -0500

    javascript addition

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,6 @@
-function myFunction() { //This initializes the function, and names it "myFunction()"
-    var sentence = "I am learning"; //This indicates the first part of the sentence to be displayed as part of the function.
-    sentence += " a lot from this book!"; //This is a continuation of the sentence through concatenating the two lines.
-    document.getElementById("Concatenate").innerHTML = sentence; //This gives the element the id "Concatenate," which allows the html to call it.
+window.alert("Testing!")
+
+function myFunction() {
+    document.write("hello world!")
 }
+document.getElementById("demo").innerHTMl = myFunction();

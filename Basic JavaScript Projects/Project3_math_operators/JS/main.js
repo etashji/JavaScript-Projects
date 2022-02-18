3ff68027dfa59c6a78f7293837467c9029fa9af5
etashji <99293073+etashji@users.noreply.github.com>
2022-02-18 11:58:41 -0500

    Added Math functions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -3,4 +3,48 @@ function addition_Function() {
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
 
-window.alert("Testing!")
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 X 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10 " + 
+    "divided in half and then subtracted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divided 25 by 6 " + 
+    "you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math7").innerHTML = -x;
+}
+
+var X = 5;
+X++;
+
+var Y = 5.25;
+Y--;
+
+
+window.alert(Math.random());
+
+window.alert(Math.random() * 100);
+
+window.alert(Math.SQRT2);
+
